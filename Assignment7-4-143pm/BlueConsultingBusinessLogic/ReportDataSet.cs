a76520111fc3a63a7581c02a1e1a645141918f63
minhtuanvo <tuan_vo@live.com.au>
2014-04-07 17:19:40 +1000

    Most recent project solution
    Includes fought SQL insert and view methods

---
+++ Assignment7-4-143pm/BlueConsultingBusinessLogic/ReportDataSet.cs
@@ -0,0 +1,24 @@
+﻿namespace BlueConsultingBusinessLogic {
+    
+    
+    public partial class ReportDataSet {
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // ReportDataSet
+            // 
+            this.EnforceConstraints = false;
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
+
+
+namespace BlueConsultingBusinessLogic.ReportDataSetTableAdapters {
+    
+    
+    public partial class ReportsTableAdapter {
+    }
+}
